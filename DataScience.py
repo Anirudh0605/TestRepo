b1929ee4c4627d656f53ca19c5656c8f64b7871f
Anirudh0605 <92162559+Anirudh0605@users.noreply.github.com>
2025-03-26 20:18:41 +0530

    Create DataScience.py

---
+++ DataScience.py
@@ -0,0 +1,23 @@
+Create a markdown cell with the title of the notebook. 
+
+Create a markdown cell for an introduction. 
+
+Create a markdown cell to list data science languages.
+
+Create a markdown cell to list data science libraries. 
+
+Create a markdown cell with a table of Data Science tools.
+
+Create a markdown cell introducing arithmetic expression examples. 
+
+Create a code cell to multiply and add numbers. 
+
+Create a code cell to convert minutes to hours. 
+
+Insert a markdown cell to list Objectives. 
+
+Create a markdown cell to indicate the Author’s name. 
+
+Share your notebook through GitHub 
+
+Take a screenshot of the first page of the notebook. 
